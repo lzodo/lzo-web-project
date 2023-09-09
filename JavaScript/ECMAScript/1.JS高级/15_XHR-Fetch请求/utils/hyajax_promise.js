6836d9c83ce555fd6cef7514da555f548b3ae79c
lzoxun-redmi <869664233@qq.com>
2023-09-10 01:47:26 +0800

    2023-09-10 01:47提交上传

--- JavaScript/ECMAScript/1.JS高级/15_XHR-Fetch请求/utils/hyajax_promise.js
+++ JavaScript/ECMAScript/1.JS高级/15_XHR-Fetch请求/utils/hyajax_promise.js
@@ -1,49 +1,46 @@
 // 练习hyajax -> axios
 function hyajax({
-  url,
-  method = "get",
-  data = {},
-  timeout = 10000,
-  headers = {}, // token
+	url,
+	method = "get",
+	data = {},
+	timeout = 10000,
+	headers = {}, // token
 } = {}) {
-  // 1.创建对象
-  const xhr = new XMLHttpRequest()
+	// 1.创建对象
+	const xhr = new XMLHttpRequest();
 
-  // 2.创建Promise
-  const promise = new Promise((resolve, reject) => {
+	// 2.创建Promise
+	const promise = new Promise((resolve, reject) => {
+		// 2.监听数据
+		xhr.onload = function () {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({ status: xhr.status, message: xhr.statusText });
+			}
+		};
 
-    // 2.监听数据
-    xhr.onload = function() {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response)
-      } else {
-        reject({ status: xhr.status, message: xhr.statusText })
-      }
-    }
+		// 3.设置类型
+		xhr.responseType = "json";
+		xhr.timeout = timeout;
 
-    // 3.设置类型
-    xhr.responseType = "json"
-    xhr.timeout = timeout
+		// 4.open方法
+		if (method.toUpperCase() === "GET") {
+			const queryStrings = [];
+			for (const key in data) {
+				queryStrings.push(`${key}=${data[key]}`);
+			}
+			url = url + "?" + queryStrings.join("&");
+			xhr.open(method, url);
+			xhr.send();
+		} else {
+			xhr.open(method, url);
+			xhr.setRequestHeader("Content-type", "application/json");
+			xhr.send(JSON.stringify(data));
+		}
+	});
 
-    // 4.open方法
-    if (method.toUpperCase() === "GET") {
-      const queryStrings = []
-      for (const key in data) {
-        queryStrings.push(`${key}=${data[key]}`)
-      }
-      url = url + "?" + queryStrings.join("&")
-      xhr.open(method, url)
-      xhr.send()
-    } else {
-      xhr.open(method, url)
-      xhr.setRequestHeader("Content-type", "application/json")
-      xhr.send(JSON.stringify(data))
-    }
-  })
+	promise.xhr = xhr;
 
-  promise.xhr = xhr
-
-  return promise
+	return promise;
 }
-
-
