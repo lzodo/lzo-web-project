1abde3e61a3ed950d8747a4aac945e6d6df17573
lzoxun-redmi <869664233@qq.com>
2023-10-02 00:35:49 +0800

    update

--- src/JavaScript/案例/指定任务个数并发执行.js
+++ src/JavaScript/案例/指定任务个数并发执行.js
@@ -50,3 +50,46 @@ execTasks(
 	),
 	3
 );
+
+// =============================================================以前实现方案
+// function limitRequest(urls = [], limit = 3) {
+//     return new Promise((resolve, reject) => {
+//         const len = urls.length;
+//         let count = 0;
+
+//         // 同时启动limit个任务
+//         while (limit > 0) {
+//             start();
+//             limit -= 1;
+//         }
+
+//         function start() {
+//             const url = urls.shift(); // 从数组中拿取第一个任务
+//             if (url) {
+//                 console.log(url);
+//                 setTimeout(() => {
+//                     if (count == len - 1) {
+//                         // 最后一个任务完成
+//                         resolve();
+//                     } else {
+//                         // 完成之后，启动下一个任务
+//                         count++;
+//                         start();
+//                     }
+//                 }, 2000);
+//             }
+//         }
+//     });
+// }
+
+// // 测试
+// limitRequest([
+//     "http://xxa",
+//     "http://xxb",
+//     "http://xxc",
+//     "http://xxd",
+//     "http://xxe",
+//     "http://xxf",
+// ]).then(() => {
+//     console.log("已经全部完成");
+// });
